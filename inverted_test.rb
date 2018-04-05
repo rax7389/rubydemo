4b596807d025ba69ad979c57c403f48c070230a9
rax7389 <rahul12bhowmick@gmail.com>
2018-04-05 16:49:53 +0530

    invertedNumbers

--- inverted_test.rb
+++ inverted_test.rb
@@ -7,7 +7,6 @@ def inverted(invert,length_loop)
   temp = invert
   while loop_cntrl < length_loop do
     rem = temp % 10
-    
     if rem == 0
       rev = rev*10 + rem
     elsif rem == 1
@@ -19,11 +18,9 @@ def inverted(invert,length_loop)
     elsif rem == 9
       rev = rev*10 + 6
     end 
-
     temp = (temp / 10).to_i
     loop_cntrl =loop_cntrl + 1
   end
-
   if rev == invert
     return true
   end
@@ -31,14 +28,12 @@ end
 
 def inverted_input(a,b)
   ttl_inver_num,retn,length_loop = 0,false 
-
   for i in (a..b)
     retn = inverted i , i.to_s.size
     
     if retn == true 
       ttl_inver_num = ttl_inver_num + 1
     end
-
     retn = false
   end
 
