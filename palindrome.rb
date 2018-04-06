e9ec14bc81268df7f6572517f07e1ca53d8d5875
rax7389 <rahul12bhowmick@gmail.com>
2018-04-06 19:01:13 +0530

    palindome

---
+++ palindrome.rb
@@ -0,0 +1,28 @@
+str = gets 
+
+
+def palidrome(str)
+	centerUsed,count,center,c = false,0,'',''
+	a,b = 0,str.size-1
+	for i in (a...b)
+		c = str[i]
+		count = 0
+		for j in (a...b)
+			if str[j] == c
+				count += 1
+			end
+			if count % 2 == 1
+				if centerUsed == true && center != c
+					return false
+				else
+					centerUsed = true
+	        center = c
+				end
+			end
+		end
+	end
+	return true
+end
+
+res = palidrome str
+puts res
